5a5fcadd988b96341d67dd27ba4f63403e46cbc6
linyujie <sheldonlinyujie@gmail.com>
2018-04-17 16:50:47 -0700

    basic idea of electron done

--- index.js
+++ index.js
@@ -1,8 +1,82 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron; //overall electron running electron app process.
+const { app, BrowserWindow, Menu, ipcMain } = electron; //overall electron running electron app process.
+
+let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on('closed', () => app.quit());
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 })
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo'
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+  addWindow.on('closed', () => addWindow = null);
+}
+
+ipcMain.on('todo:add', (event, todo) => {
+  mainWindow.webContents.send('todo:add', todo);
+  addWindow.close();
+})
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Todo',
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Clear Todos',
+        click() {
+          mainWindow.webContents.send('todo:clear');
+        }
+     },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      },
+    ]
+  }
+];
+
+if (process.platform == 'darwin') {
+  menuTemplate.unshift({});
+}
+
+
+//'PRODUCTION'
+//'TEST'
+//'DEVELOPMENT'
+//
+if (process.env.NODE_ENV !== 'production') {
+  menuTemplate.push({
+    label: 'View',
+    submenu: [
+      { role: 'reload'},
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  })
+}
